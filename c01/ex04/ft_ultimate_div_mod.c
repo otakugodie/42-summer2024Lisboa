3b181cf03f69851531734e7a10e0a53a424c974b
otakugodie <diegofer.rodriguezc@gmail.com>
2024-09-23 17:42:49 +0100

    Sync ex00 to ex04

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -1,27 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: diegfern <diegfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 13:17:24 by otakugodie        #+#    #+#             */
-/*   Updated: 2024/09/23 11:54:48 by diegfern         ###   ########.fr       */
+/*   Created: 2024/09/23 19:15:01 by diegfern          #+#    #+#             */
+/*   Updated: 2024/09/23 20:39:36 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*nbr = 42;
+	int	div;
+	int	mod;
+
+	div = *a;
+	mod = *b;
+	*a = div / mod;
+	*b = div % mod;
 }
+
+/*
+#include <stdio.h>
 
 int	main(void)
 {
-	int	school;
-	int	*nbr;
+	int	a;
+	int	b;
 
-	school = 0;
-	nbr = &school;
-	ft_ft(nbr);
+	a = 42;
+	b = 10;
+	printf("a= %d, b= %d \n", a, b);
+	ft_ultimate_div_mod(&a, &b);
+	printf("a= %d, b= %d \n", a, b);
 	return (0);
 }
+*/
