5878aa4aad7b70c69565ea053e075e4db6456917
otakugodie <diegofer.rodriguezc@gmail.com>
2024-10-09 11:08:45 +0100

    Sync c07 and C08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 00:49:34 by diegfern          #+#    #+#             */
+/*   Updated: 2024/10/09 00:49:56 by diegfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define TRUE 1
+# define FALSE 0
+# define EVEN(nbr) nbr % 2 == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
