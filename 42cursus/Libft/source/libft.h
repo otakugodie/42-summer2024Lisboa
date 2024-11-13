32f01e0109b31e78cf178bd4c91a11cbc87d1ff8
otakugodie <diegofer.rodriguezc@gmail.com>
2024-11-13 18:27:53 +0000

    Add ft_strdup

--- 42cursus/Libft/source/libft.h
+++ 42cursus/Libft/source/libft.h
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 18:45:50 by diegfern          #+#    #+#             */
-/*   Updated: 2024/11/12 20:34:21 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/11/13 18:27:01 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,12 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
-void    *ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
-
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
 
-
-
+char	*ft_strdup(const char *s);
 
 #endif
