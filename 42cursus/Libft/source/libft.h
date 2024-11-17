9c97ec9a3b917cdc5bcb4b1bb4e27cfbd2382a8e
otakugodie <diegofer.rodriguezc@gmail.com>
2024-11-17 14:32:49 +0000

    Sync strmapi

--- 42cursus/Libft/source/libft.h
+++ 42cursus/Libft/source/libft.h
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 18:45:50 by diegfern          #+#    #+#             */
-/*   Updated: 2024/11/16 19:52:50 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/11/16 23:23:57 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
