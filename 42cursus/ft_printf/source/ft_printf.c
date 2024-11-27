829e9298af414f197e3c9478c3f2aca9d21ddcf6
otakugodie <diegofer.rodriguezc@gmail.com>
2024-11-27 19:36:10 +0000

    Test variadic function

--- 42cursus/ft_printf/source/ft_printf.c
+++ 42cursus/ft_printf/source/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 21:23:17 by diegfern          #+#    #+#             */
-/*   Updated: 2024/11/26 20:09:26 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/11/27 18:19:20 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,35 @@
 
 int	ft_printf(const char *, ...);
 
+int sumatoria (int n, ...){
+	int	i;
+	int	ac;
+	int	sig;
+
+	ac = 0;
+	
+	va_list ap;		//ap> argument pointer
+	va_start(ap, n);
+
+	i = 0;
+	printf("Iniciamos argumentos\n");
+	while (i < n){		
+		sig = va_arg(ap, int);
+		printf("El siguiente argumento es: %d\n", sig);
+		ac += sig;
+		i++;
+	}
+
+	va_end(ap);
+	return (ac);
+}
+
 int	main(void)
 {
-	/* code */
+	int	total;
+
+	total = sumatoria(3, 1,2,3);
+	printf ("Total: %d\n", total);
+
 	return (0);
 }
--- 42cursus/ft_printf/source/ft_printf.h
+++ 42cursus/ft_printf/source/ft_printf.h
@@ -6,12 +6,15 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 21:24:09 by diegfern          #+#    #+#             */
-/*   Updated: 2024/11/26 20:08:49 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/11/27 13:17:51 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int	ft_printf(const char *, ...);
 
