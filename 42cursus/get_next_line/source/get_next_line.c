1e95c5b13a8fdae65d42649473ef1bfb4f2d5591
otakugodie <diegofer.rodriguezc@gmail.com>
2024-12-08 15:14:10 +0000

    First advance get_next_line read a file

--- 42cursus/get_next_line/source/get_next_line.c
+++ 42cursus/get_next_line/source/get_next_line.c
@@ -6,10 +6,33 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:07:48 by diegfern          #+#    #+#             */
-/*   Updated: 2024/12/03 19:11:32 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/12/08 15:10:48 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+
+int	main(void)
+{
+	int		fd;
+	char	buffer[10];
+	ssize_t	num_bytes;
+
+	fd = open("/home/otakugodie/projects42/42cursus/get_next_line/source/testfile",
+			O_RDONLY);
+	if (fd == -1)
+		printf("Error trying open file \n");
+	else
+	{
+		num_bytes = read(fd, buffer, 5);
+		close(fd);
+		if (num_bytes==0)
+			printf("File is empty\n");
+		else
+			printf("Number of characters is: %ld, and his contents is: %s\n",
+				num_bytes, buffer);
+	}
+	return (0);
+}
--- 42cursus/get_next_line/source/get_next_line.h
+++ 42cursus/get_next_line/source/get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:08:21 by diegfern          #+#    #+#             */
-/*   Updated: 2024/12/03 19:10:51 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/12/08 14:55:33 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
 
+
+char	*get_next_line(int fd);
 
 #endif
