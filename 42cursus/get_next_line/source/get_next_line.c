6e851edc5d28677cab8ac91b008b454df6c500ee
otakugodie <diegofer.rodriguezc@gmail.com>
2024-12-09 22:44:54 +0000

    Sync read working with Null terminator

--- 42cursus/get_next_line/source/get_next_line.c
+++ 42cursus/get_next_line/source/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:07:48 by diegfern          #+#    #+#             */
-/*   Updated: 2024/12/09 20:04:06 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/12/09 22:41:10 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,24 @@ char	*get_next_line(int fd);
 int	main(void)
 {
 	int		fd;
-	int		i;
-	char	buffer[10];
+	char	buffer[BUFFER_SIZE + 1];
 	ssize_t	num_bytes;
 
-	fd = open("/home/otakugodie/projects42/42cursus/get_next_line/source/testfile",
-			O_RDONLY);
+	fd = open("testfile", O_RDONLY);
 	if (fd == -1)
-		printf("Error trying open file \n");
-	else
 	{
-		i = 0;
-		while (i <= 3 ){
-			num_bytes = read(fd, buffer, 5);
-			
-			if (num_bytes == 0)
-				printf("File is empty\n");
-			else
-				printf("Number of characters is: %ld, and his contents is: %s\n", num_bytes, buffer);
-			i++;
-		}
-		close(fd);
+		printf("Error trying open file \n");
+		return (1);
 	}
+	num_bytes = read(fd, buffer, BUFFER_SIZE);
+	while (num_bytes > 0)
+	{
+		buffer[num_bytes] = '\0';
+		printf("Bytes read: %ld, Content: \"%s\"\n", num_bytes, buffer);
+		num_bytes = read(fd, buffer, BUFFER_SIZE);
+	}
+	if (num_bytes == -1)
+		printf("Error: Unable to read file\n");
+	close(fd);
 	return (0);
 }
--- 42cursus/get_next_line/source/get_next_line.h
+++ 42cursus/get_next_line/source/get_next_line.h
@@ -6,17 +6,18 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 19:08:21 by diegfern          #+#    #+#             */
-/*   Updated: 2024/12/08 14:55:33 by diegfern         ###   ########.fr       */
+/*   Updated: 2024/12/09 22:23:11 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFFER_SIZE 3
+
+# include <fcntl.h>
 # include <stdio.h>
 # include <unistd.h>
-# include <fcntl.h>
-
 
 char	*get_next_line(int fd);
 
