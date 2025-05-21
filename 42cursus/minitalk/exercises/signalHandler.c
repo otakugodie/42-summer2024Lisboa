f640fdae0d8c51101b9ce6ee7f2d330e7febcdc0
otakugodie <diegofer.rodriguezc@gmail.com>
2025-05-21 09:01:38 +0100

    Test signalHandler to handle custom signals

--- 42cursus/minitalk/exercises/signalHandler.c
+++ 42cursus/minitalk/exercises/signalHandler.c
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 18:00:21 by diegfern          #+#    #+#             */
-/*   Updated: 2025/05/20 17:53:05 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/05/21 08:57:15 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,43 @@
 // sighandler_t  signal(int  signum, sighandler_t handler);
 
 void ft_signalHandler(int sig){
-	printf("Signal is: %d \n", sig);
+	if (sig == SIGUSR1){
+		printf("Signal is: %d \n", sig);
+	}
+	else if(sig == SIGUSR2){
+		printf("Signal is: %d \n", sig);	
+	}
+	else {
+		printf("Signal ignored \n");
+	}
+	
 }
 
 int main (void){
 
-	//signal(SIGIO, &ft_signalHandler);
-	signal (SIGALRM, SIG_IGN);	//Modifico el Signal disposition por defecto, para este caso es Term sino hay un signal handler
+	int i;
+	
+	signal(SIGUSR1, &ft_signalHandler);
+	signal(SIGUSR2, &ft_signalHandler);
+
+	i = 1;
+
+	while (i <= 31){
+		if (i != SIGUSR1 && i != SIGUSR2 && i!= SIGKILL && i != SIGSTOP && i != SIGINT ){
+			//SIGINT la uso para terminar el proceso con Ctrl+C
+			signal(i, &ft_signalHandler);
+		}
+		i++;
+	}
+
+	//signal (SIGALRM, SIG_IGN);	//Modifico el Signal disposition por defecto, para este caso es Term (Terminar) sino hay un signal handler. (Hay Term, Core, Ign, Stop, Cont)
+	printf("PID server is: %d \n", getpid());
+	
 	while (1){
-		printf("...Awaiting signals...\n");
-		sleep(2);
+		//printf("...Awaiting signals...\n");
+		//printf("PID server is: %d \n", getpid());
+		//sleep(2);
+		pause();
 	}
 	return 0;
 }
