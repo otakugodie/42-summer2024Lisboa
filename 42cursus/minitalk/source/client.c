b410d5685a22c40820980db9f6b4366502c1ea32
otakugodie <diegofer.rodriguezc@gmail.com>
2025-05-21 23:01:30 +0100

    Test 1 with client

--- 42cursus/minitalk/source/client.c
+++ 42cursus/minitalk/source/client.c
@@ -6,7 +6,26 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:27:06 by diegfern          #+#    #+#             */
-/*   Updated: 2025/02/01 08:41:45 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/05/21 22:59:54 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main (int arg, char *argv[]){
+	if (arg >= 3){
+		printf("PID process is: %s\n", argv[1]);
+		printf("Message is: %s\n", argv[2]);
+		while (1){
+			kill (14867, SIGUSR1);
+			sleep(2);
+			kill (14867, SIGUSR2);
+			sleep(2);
+		}
+	}
+	
+
+	return (0);
+}
