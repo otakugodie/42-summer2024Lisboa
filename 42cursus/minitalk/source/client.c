91fdcde81bb5bffd9e270d1a1aabbde385ec2c51
otakugodie <diegofer.rodriguezc@gmail.com>
2025-05-30 21:52:18 +0100

    Test send signals with chars

--- 42cursus/minitalk/source/client.c
+++ 42cursus/minitalk/source/client.c
@@ -6,21 +6,51 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:27:06 by diegfern          #+#    #+#             */
-/*   Updated: 2025/05/30 21:29:06 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/05/30 21:51:38 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (int arg, char *argv[]){
+void send_char_to_server(int pid_server, char c) {
+    int bit;
+
+    for (bit = 7; bit >= 0; bit--) {
+        if ((c >> bit) & 1) {
+            kill(pid_server, SIGUSR2); // Enviar bit 1 como SIGUSR2
+        } else {
+            kill(pid_server, SIGUSR1); // Enviar bit 0 como SIGUSR1
+        }
+        usleep(100); // Pausa breve para que el servidor procese la señal
+    }
+}
+
+int main (int argc, char *argv[]){
 
 	int pid_server;
+	char *message;
+    int i;
 
-	pid_server = 23823;
+	//pid_server = 31866;
 
-	if (arg >= 3){
+	if (argc != 3) {
+        write(2, "Usage: ./client <PID_SERVER> <MESSAGE>\n", 39);
+        return (1);
+    }
+
+	pid_server = atoi(argv[1]);
+	message = argv[2];
+
+	i = 0;
+    while (message[i]) {
+        send_char_to_server(pid_server, message[i]); // Enviar cada carácter
+        i++;
+    }
+
+	/* if (argc >= 3){
 		printf("PID process is: %s\n", argv[1]);
 		printf("Message is: %s\n", argv[2]);
 		while (1){
@@ -29,8 +59,7 @@ int main (int arg, char *argv[]){
 			kill (pid_server, SIGUSR2);
 			sleep(2);
 		}
-	}
-	
+	} */
 
 	return (0);
 }
--- 42cursus/minitalk/source/server.c
+++ 42cursus/minitalk/source/server.c
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:27:29 by diegfern          #+#    #+#             */
-/*   Updated: 2025/05/30 21:29:04 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/05/30 21:51:37 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
+static int g_bit_count = 0; // Contador de bits recibidos
+static char g_char = 0;    // Carácter reconstruido
+
 void ft_signalHandler (int sig){
 	if (sig == SIGUSR1){
-		printf("Signal is: %d \n", sig);
+		//printf("Signal is: %d \n", sig);
+		g_char = (g_char << 1) | 0; // Desplaza y añade un 0
 	}
 	else if(sig == SIGUSR2){
-		printf("Signal is: %d \n", sig);	
+		//printf("Signal is: %d \n", sig);
+		g_char = (g_char << 1) | 1; // Desplaza y añade un 1
 	}
 	else {
 		printf("Signal ignored \n");
 	}
+	g_bit_count++;
+	if (g_bit_count == 8) { // Cuando se reciben 8 bits
+        write(1, &g_char, 1); // Imprime el carácter reconstruido
+        g_bit_count = 0;      // Reinicia el contador
+        g_char = 0;           // Reinicia el carácter
+		write(1, "\n", 1);
+    }
 }
 
 int main (void){
