82a300895db71d1419a9d2609d9356fa00278293
otakugodie <diegofer.rodriguezc@gmail.com>
2025-06-02 22:27:46 +0100

    Apply norminette to files

--- 42cursus/minitalk/source/client.c
+++ 42cursus/minitalk/source/client.c
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:27:06 by diegfern          #+#    #+#             */
-/*   Updated: 2025/05/31 23:51:01 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/06/02 22:12:05 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,48 @@
 //#include <stdlib.h>
 #include "libft/libft.h"
 
-void send_char_to_server(int pid_server, char c) {
-    int bit;
+void	send_char_to_server(int pid_server, char c)
+{
+	int	bit;
 
-    for (bit = 7; bit >= 0; bit--) {
-        if ((c >> bit) & 1) {
-            kill(pid_server, SIGUSR2); // Enviar bit 1 como SIGUSR2
-        } else {
-            kill(pid_server, SIGUSR1); // Enviar bit 0 como SIGUSR1
-        }
-        usleep(100); // Pausa breve para que el servidor procese la señal
-    }
+	bit = 7;
+	while (bit >= 0)
+	{
+		if ((c >> bit) & 1)
+		{
+			kill(pid_server, SIGUSR2); // Enviar bit 1 como SIGUSR2
+		}
+		else
+		{
+			kill(pid_server, SIGUSR1); // Enviar bit 0 como SIGUSR1
+		}
+		usleep(100); // Pausa breve para que el servidor procese la señal
+		bit--;
+	}
 }
 
-int main (int argc, char *argv[]){
-
-	int pid_server;
-	char *message;
-    int i;
-
+int	main(int argc, char *argv[])
+{
+	int		pid_server;
+	char	*message;
+	int		i;
 	//pid_server = 31866;
-
-	if (argc != 3) {
-        write(2, "Usage: ./client <PID_SERVER> <MESSAGE>\n", 39);
-        return (1);
-    }
-
+	if (argc != 3)
+	{
+		write(2, "Usage: ./client <PID_SERVER> <MESSAGE>\n", 39);
+		return (1);
+	}
 	pid_server = ft_atoi(argv[1]);
 	//ft_printf("Sending message to server...\n");
 	message = argv[2];
-
 	i = 0;
-    while (message[i]) {
-        send_char_to_server(pid_server, message[i]); // Enviar cada carácter
-        i++;
-    }
-
+	while (message[i])
+	{
+		send_char_to_server(pid_server, message[i]); // Enviar cada carácter
+		i++;
+	}
+	send_char_to_server(pid_server, '\0'); // Enviar carácter nulo al final
+	usleep(500);
 	/* if (argc >= 3){
 		ft_printf ("PID process is: %s\n", argv[1]);
 		ft_printf ("Message is: %s\n", argv[2]);
@@ -62,6 +68,5 @@ int main (int argc, char *argv[]){
 			sleep(2);
 		}
 	} */
-
 	return (0);
 }
--- 42cursus/minitalk/source/server.c
+++ 42cursus/minitalk/source/server.c
@@ -6,59 +6,74 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:27:29 by diegfern          #+#    #+#             */
-/*   Updated: 2025/05/31 23:52:27 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/06/02 22:27:11 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "libft/libft.h"
 
-static int g_bit_count = 0; // Contador de bits recibidos
-static char g_char = 0;    // Carácter reconstruido
+static int	g_bit_count = 0; // Contador de bits recibidos
+static char	g_char = 0;	// Carácter reconstruido
 
-void ft_signalHandler (int sig){
-	if (sig == SIGUSR1){
-		//printf("Signal is: %d \n", sig);
+void	ft_signal_handler(int sig)
+{
+	if (sig == SIGUSR1)
+	{
+		// printf("Signal is: %d \n", sig);
 		g_char = (g_char << 1) | 0; // Desplaza y añade un 0
+		g_bit_count++;
 	}
-	else if(sig == SIGUSR2){
-		//printf("Signal is: %d \n", sig);
+	else if (sig == SIGUSR2)
+	{
+		// printf("Signal is: %d \n", sig);
 		g_char = (g_char << 1) | 1; // Desplaza y añade un 1
+		g_bit_count++;
 	}
-	else {
+	else
+	{
 		ft_printf("Signal ignored \n");
+		return ;
 	}
-	g_bit_count++;
-	if (g_bit_count == 8) { // Cuando se reciben 8 bits
-        write(1, &g_char, 1); // Imprime el carácter reconstruido
-        g_bit_count = 0;      // Reinicia el contador
-        g_char = 0;           // Reinicia el carácter
-		write(1, "\n", 1);
-    }
+	if (g_bit_count == 8)
+	{
+		if (g_char == '\0')
+		{
+			// Si el carácter es nulo, termina el mensaje
+			write(1, "\nMessage received successfully!\n", 32);
+			g_bit_count = 0;
+			g_char = 0;
+			return ;
+		}
+		// Cuando se reciben 8 bits
+		write(1, &g_char, 1); // Imprime el carácter reconstruido
+		g_bit_count = 0; // Reinicia el contador
+		g_char = 0; // Reinicia el carácter
+	}
 }
 
-int main (void){
+int	main(void)
+{
+	int	i;
 
 	ft_printf("PID server is: %d \n", getpid());
-
-	while (1){
-		int i;
-	
-		signal(SIGUSR1, &ft_signalHandler);
-		signal(SIGUSR2, &ft_signalHandler);
-
+	while (1)
+	{
+		signal(SIGUSR1, &ft_signal_handler);
+		signal(SIGUSR2, &ft_signal_handler);
 		i = 1;
-
-		while (i <= 31){
-			if (i != SIGUSR1 && i != SIGUSR2 && i!= SIGKILL && i != SIGSTOP && i != SIGINT ){
-				//SIGINT la uso para terminar el proceso con Ctrl+C
-				signal(i, &ft_signalHandler);
+		while (i <= 31)
+		{
+			if (i != SIGUSR1 && i != SIGUSR2 && i != SIGKILL && i != SIGSTOP
+				&& i != SIGINT)
+			{
+				// SIGINT la uso para terminar el proceso con Ctrl+C
+				signal(i, &ft_signal_handler);
 			}
 			i++;
 		}
 	}
-
 	return (0);
 }
