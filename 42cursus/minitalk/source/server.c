778d8039c610c4dc974c44385c4687354fdd2c59
otakugodie <diegofer.rodriguezc@gmail.com>
2025-06-05 20:15:43 +0100

    Sync files without comments

--- 42cursus/minitalk/source/server.c
+++ 42cursus/minitalk/source/server.c
@@ -6,7 +6,7 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:27:29 by diegfern          #+#    #+#             */
-/*   Updated: 2025/06/04 17:24:59 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/06/05 20:12:47 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,34 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static int	g_bit_count = 0; // Contador de bits recibidos
-static char	g_char = 0;	// Carácter reconstruido
+static int	g_bit_count = 0;
+static char	g_char = 0;
 
 void	ft_signal_handler(int sig)
 {
 	if (sig == SIGUSR1)
 	{
-		// printf("Signal is: %d \n", sig);
-		g_char = (g_char << 1) | 0; // Desplaza y añade un 0
+		g_char = (g_char << 1) | 0;
 		g_bit_count++;
 	}
 	else if (sig == SIGUSR2)
 	{
-		// printf("Signal is: %d \n", sig);
-		g_char = (g_char << 1) | 1; // Desplaza y añade un 1
+		g_char = (g_char << 1) | 1;
 		g_bit_count++;
-	}
-	else
-	{
-		ft_printf("Signal ignored \n");
-		return ;
 	}
 	if (g_bit_count == 8)
 	{
 		if (g_char == '\0')
 		{
-			// Si el carácter es nulo, termina el mensaje
-			write(1, "\nMessage received successfully!\n", 32);
 			g_bit_count = 0;
 			g_char = 0;
 			return ;
 		}
-		// Cuando se reciben 8 bits
-		write(1, &g_char, 1); // Imprime el carácter reconstruido
-		g_bit_count = 0; // Reinicia el contador
-		g_char = 0; // Reinicia el carácter
+		write(1, &g_char, 1);
+		g_bit_count = 0;
+		g_char = 0;
 	}
+	return ;
 }
 
 int	main(void)
@@ -69,7 +60,6 @@ int	main(void)
 			if (i != SIGUSR1 && i != SIGUSR2 && i != SIGKILL && i != SIGSTOP
 				&& i != SIGINT)
 			{
-				// SIGINT la uso para terminar el proceso con Ctrl+C
 				signal(i, &ft_signal_handler);
 			}
 			i++;
