0a2999026e9bd7f6be886a1e5c5bf24fba8a34b1
otakugodie <diegofer.rodriguezc@gmail.com>
2025-06-29 19:52:14 +0100

    Working close Window using X(Window) and ESC using hook

---
+++ 42cursus/fdf/exercises/ft_memset.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 18:29:09 by diegfern          #+#    #+#             */
+/*   Updated: 2024/11/13 18:29:27 by diegfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
+
+/* #include <stdio.h>
+
+int	main(void)
+{
+	char	str[50] = "This is memset function";
+
+	printf("String is: %s\n", str);
+	ft_memset(str, '0', 5);
+	printf("String is: %s\n", str);
+	return (0);
+}
+ */
--- 42cursus/fdf/exercises/mlx_test.c
+++ 42cursus/fdf/exercises/mlx_test.c
@@ -1,4 +1,25 @@
 #include "mlx.h"
+#include "libft.h"
+
+void render (void *param){
+	//mlx_pixel_put(mlx, win, 200, 151, 0xFFFF00);
+	//mlx_pixel_put(mlx, win, 200, 152, 0xFFFFFF);	//Blanco
+	//mlx_pixel_put(mlx, win, 200, 153, 0x000000);	//Negro
+}
+
+int	close_window(void *param)
+{
+    (void)param;
+    exit(0);
+    return (0);
+}
+
+int	key_hook(int keycode, void *param)
+{
+    if (keycode == 65307) // ESC en Linux
+        exit(0);
+    return (0);
+}
 
 int main(void)
 {
@@ -8,16 +29,39 @@ int main(void)
 
     mlx = mlx_init();
 	win = mlx_new_window(mlx, 950, 540, "Hello world!");
-	mlx_pixel_put(mlx, win, 200, 150, 0xFF0000);
-	mlx_pixel_put(mlx, win, 200, 151, 0x00FF00);
-	mlx_pixel_put(mlx, win, 200, 152, 0x0000FF);
+	
+	
+	//mlx_pixel_put(mlx, win, 200, 150, 0xFF0000);
+	//mlx_pixel_put(mlx, win, 200, 151, 0x00FF00);
+	//mlx_pixel_put(mlx, win, 200, 152, 0x0000FF);
 
+	//Dibuja linea horizontal
 	for (i = 0; i < 100; i++)
         mlx_pixel_put(mlx, win, 100 + i, 100, 0x0000FF);
 
+	
+	//Dibuja linea vertical
+	i = 0;
+	for (i = 0; i < 100; i++)
+        mlx_pixel_put(mlx, win, 200, 200+i, 0x00FF00);
+
+	//Dibuja linea perpendicular
+	i = 0;
+	for (i = 0; i < 100; i++)
+        mlx_pixel_put(mlx, win, 300+i, 300+i, 0xFF0000);
+	
+	
+	mlx_pixel_put(mlx, win, 0, 0, 0xFF0000);
+	mlx_pixel_put(mlx, win, 150, 150, 0xFFFFFF);
+	mlx_pixel_put(mlx, win, 300, 300, 0xFFFFFF);
+	
 	//mlx_pixel_put(mlx, win, 200, 151, 0xFFFF00);
 	//mlx_pixel_put(mlx, win, 200, 152, 0xFFFFFF);	//Blanco
 	//mlx_pixel_put(mlx, win, 200, 153, 0x000000);	//Negro
+
+	mlx_hook(win, 17, 0, close_window, NULL);      // Cerrar con la X
+    mlx_hook(win, 2, 1L<<0, key_hook, NULL);       // Cerrar con ESC
+
     mlx_loop(mlx);
     return (0);
 }
