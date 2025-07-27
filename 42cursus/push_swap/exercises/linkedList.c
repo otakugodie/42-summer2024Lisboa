aa0b4df03f9900d3eda2f26d574f7d134ef5495d
otakugodie <diegofer.rodriguezc@gmail.com>
2025-07-27 17:50:08 +0100

    Add first struct linked list

--- 42cursus/push_swap/exercises/linkedList.c
+++ 42cursus/push_swap/exercises/linkedList.c
@@ -6,10 +6,19 @@
 /*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 18:37:19 by diegfern          #+#    #+#             */
-/*   Updated: 2025/07/25 21:56:37 by diegfern         ###   ########.fr       */
+/*   Updated: 2025/07/27 17:49:29 by diegfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // [#, &siguiente_nodo]
+
+typedef struct s_linkedList
+{
+	int value;
+	int index;
+	struct s_linkedList *next;
+
+} t_linkedList;
+
 
 //Radix algorithm
