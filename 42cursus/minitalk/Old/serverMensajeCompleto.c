618188c7a88885ab53ab73f823ce0502e8caaab5
otakugodie <diegofer.rodriguezc@gmail.com>
2025-08-21 11:26:54 +0100

    Test server minitalk

---
+++ 42cursus/minitalk/Old/serverMensajeCompleto.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegfern <diegfern@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:27:29 by diegfern          #+#    #+#             */
+/*   Updated: 2025/08/16 17:26:42 by diegfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd((n / 10), fd);
+	}
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
+}
+
+void	signal_handler(int sig, siginfo_t *info, void *ucontext)
+{
+	static char	c;
+	static int	bit_count;
+	static char	buffer[1024];
+	static int	msg_index;
+
+	(void)ucontext;
+	if (sig == SIGUSR1)
+		c = (c << 1) | 0;
+	else if (sig == SIGUSR2)
+		c = (c << 1) | 1;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		if (c == '\0')
+		{
+			buffer[msg_index] = '\0';
+			write(1, buffer, msg_index);
+			write(1, "\n", 1);
+			msg_index = 0;
+			kill((*info).si_pid, SIGUSR2);
+			bit_count = 0;
+			c = 0;
+			return ;
+		}
+		if (msg_index < 1023)
+		{
+			buffer[msg_index] = c;
+			msg_index++;
+		}
+		bit_count = 0;
+		c = 0;
+	}
+	kill((*info).si_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	write(1, "Server PID is: ", 15);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 2);
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sa.sa_sigaction = signal_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
+}
